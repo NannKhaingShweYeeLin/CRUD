5d055a58fb0dc325f2b0446609ea1cc4b811adda
Nann Khaing Shwe Yee Lin <nannkhaingshweyeelin@gmail.com>
2024-02-05 09:28:11 +0630

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+    {
+        id: "1",
+        Name: "Nann Khaing",
+        Age: "20"
+    },{
+        id: "2",
+        Name: "Shwe Yee",
+        Age: "22"
+    },{
+        id: "3",
+        Name: "Yee Lin",
+        Age: "23"
+    }
+]
+
+export default Employees;
